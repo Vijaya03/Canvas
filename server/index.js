3b325df4517763d00924194ebec3fc44dcad3742
Vijaya Maurya <vijayamaurya@Vijayas-MacBook-Air.local>
2025-12-25 01:30:19 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({port:8080});
+const rooms = new Map();
+wss.on('connection',(socket)=>{
+    console.log('Client connected');
+    socket.on('message',(raw)=>{
+        const message = JSON.parse(raw.toString());
+
+        // Join Room
+        if(message.type === 'JOIN_ROOM'){
+            const {roomId} = message;
+            socket.roomId = roomId
+            if(!rooms.has(roomId)){
+                rooms.set(roomId, new Set())
+            }
+            rooms.get(roomId).add(socket);
+            return;
+        }
+
+        // draw event
+        if(message.type === 'DRAW'){
+            const {x,y} = message;
+            const roomId = socket.roomId;
+            if(!roomId) return;
+
+            rooms.get(roomId)?.forEach((client)=>{
+                if(client!== socket && client.readyState === WebSocket.OPEN){
+                client.send(raw.toString());
+            }
+        })
+        }
+    })
+
+    socket.on('close',()=>{
+        console.log('Client disconnected');
+        const roomId = socket.roomId;
+        if(roomId && rooms.has(roomId)){
+            rooms.get(roomsId).delete(socket);
+            if(rooms.get(roomId).size ===0){
+                rooms.delete(roomId)
+            }
+        }
+    })
+})
+
+console.log('WebSocket server is running on ws://localhost:8080');
